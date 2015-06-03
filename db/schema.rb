c79903782aca83b2bd67e3e0493ca206f4ad3b7a
alexpolushkin <polushkinalex@gmail.com>
2015-06-03 19:38:39 -0400

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150603232533) do
+
+  create_table "subscribers", force: :cascade do |t|
+    t.string   "phone_number"
+    t.boolean  "subscribed"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
