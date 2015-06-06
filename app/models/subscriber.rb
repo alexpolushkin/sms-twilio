cff184f7b32840c881a8f34860574b9bd5a0ba6a
alexpolushkin <polushkinalex@gmail.com>
2015-06-06 19:48:49 -0400

    update

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,7 +1,7 @@
 class Subscriber < ActiveRecord::Base
 
   def send_message(msg, image_url)
-    @twilio_number = ENV['+19179096072']
+    @twilio_number = ENV['TWILIO_NUMBER']
     @client = Twilio::REST::Client.new ENV['AC127af8098ad486a9dab8f83014b5c74b'], ENV['871173ec60de644f4d8ae6f854b8a2fa']
     message = @client.account.messages.create(
       :from => @twilio_number,
